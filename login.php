e8e56e7e6eb8366f06f7213d8af99823f21fa68d
JasirLimani <jasirlimani12@gmail.com>
2023-12-09 18:38:28 +0100

    made progress

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyBook</title>
+    <link rel="stylesheet" href="loginStyle.css">
+</head>
+<body>
+    <div class="bar">
+        <h1 class="headerText">MyBook</h1>
+        <p class="signupText">Signup</p>
+    </div>
+</body>
+</html>
