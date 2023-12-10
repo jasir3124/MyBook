8186ffaf80701ab4eab8e27aedfbc87db6c41544
JasirLimani <jasirlimani12@gmail.com>
2023-12-10 19:03:32 +0100

    made progress

--- login.php
+++ login.php
@@ -11,5 +11,14 @@
         <h1 class="headerText">MyBook</h1>
         <p class="signupText">Signup</p>
     </div>
+
+    <div class="loginFormCont">
+        <div class="loginForm"> 
+            <p class="loginText">Log in to MyBook</p><br><br>
+            <input type="text" class="inputForm" placeholder="Email"><br><br>
+            <input type="password" class="inputForm" placeholder="Password"><br><br>
+            <input type="submit" class="submitBtn" value="Log in">
+        </div>
+    </div>
 </body>
 </html>
