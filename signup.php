1bb785273b03bbc57cbfbcbf07b32eb226794ab6
JasirLimani <jasirlimani12@gmail.com>
2023-12-10 21:19:56 +0100

    made progress

---
+++ signup.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyBook | Sign up</title>
+    <link rel="stylesheet" href="signupStyle.css">
+</head>
+<body>
+    <div class="bar">
+        <h1 class="headerText">MyBook</h1>
+        <p class="signupText">Log in</p>
+    </div>
+
+    <div class="loginFormCont">
+        <div class="loginForm"> 
+            <p class="loginText">Sign up to MyBook</p><br><br>
+            <input type="text" class="inputForm" placeholder="First name"><br><br>
+            <input type="text" class="inputForm" placeholder="Last name"><br><br>
+            <p>Gender:</p> <br>
+            <select name="" id="" class="selectForm">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select><br><br>
+            <input type="text" class="inputForm" placeholder="Email"><br><br>
+            <input type="password" class="inputForm" placeholder="Password"><br><br>
+            <input type="password" class="inputForm" placeholder="Rewrite Password"><br><br>
+            <input type="submit" class="submitBtn" value="Sign up">
+        </div>
+    </div>
+</body>
+</html>
