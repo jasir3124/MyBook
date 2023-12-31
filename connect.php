e7653ff6ab3981c22660d4f223879d18f8804514
JasirLimani <jasirlimani12@gmail.com>
2023-12-31 13:42:07 +0100

    jmade progress

--- connect.php
+++ connect.php
@@ -1,20 +1,59 @@
 <?php
-    $host = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbName = "mybook";
-
-    $conn = mysqli_connect($host, $username, $password, $dbName);
 
 
-    $query = "SELECT * FROM users";
 
-    $result = mysqli_query($conn, $query);
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<pre>";
-        print_r($row);
-        echo "</pre>";
+class DataBase{
+
+
+    private $password = '';
+    private $host = 'localhost';
+    private $username = 'root';
+    private $dbName = "mybook";
+
+
+
+    function connect(){
+        $conn = mysqli_connect($this->host, $this->username, $this->password, $this->dbName);
+        return $conn;
     }
+
+    function read($query){
+        $conn = $this->connect();
+
+        $result = mysqli_query($conn, $query);
+
+        if($result == false){
+            return false;
+        } else{
+            $data = [];
+            while ($row = mysqli_fetch_assoc($result)) {
+                $data[] = $row;
+            }
+            return $data;
+        }
+    }
+
+    function save($query){
+        $conn = $this->connect();
+
+        $result = mysqli_query($conn, $query);
+
+        if($result == false){
+            return false;
+        } else{
+            return true;
+        }
+    }
+}
+
+
+
+
+$DB = new DataBase();
+
+$data = $DB->read('SELECT * FROM users');
+
+print_r($data);
 ?>
 
